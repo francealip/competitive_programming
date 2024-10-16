4d4f6dda9cae0ea3edfb70aad4b404636aa00747
francealip <f.aliprandi@studenti.unipi.it>
2024-10-16 18:40:16 +0200

    solved max_path sum adding the handling of trees with no path between two leaf

--- handson1/src/lib.rs
+++ handson1/src/lib.rs
@@ -77,7 +77,6 @@ impl Tree {
     /// A private recursive function that check if a
     /// subtree rooted at `node_id` is a BST
     fn rec_is_bst(&self, node_id: Option<usize>) -> (bool, u32, u32) {
-
         if let Some(id) = node_id {
             assert!(id < self.nodes.len(), "Node id is out of range");
             let node: &Node = &self.nodes[id];
@@ -98,19 +97,18 @@ impl Tree {
     the sum of the maximum simple path connecting two leaves. */
 
     /// return the maximum path sum
-    pub fn max_path_sum(&self) -> u32 {
-        self.rec_max_path_sum(Some(0)).0
+    pub fn max_path_sum_backup(&self) -> u32 {
+        self.rec_max_path_sum_backup(Some(0)).0
     }
 
     /// A private recursive function that return the maximum path sum and
     /// the maximum leaf-node path cost for a subtree rooted at `node_id`
-    fn rec_max_path_sum(&self, node_id: Option<usize>) -> (u32, u32) {
-
+    fn rec_max_path_sum_backup(&self, node_id: Option<usize>) -> (u32, u32) {
         if let Some(id) = node_id {
             assert!(id < self.nodes.len(), "Node id is out of range");
             let node: &Node = &self.nodes[id];
-            let (best_l, max_l) = self.rec_max_path_sum(node.id_left);
-            let (best_r, max_r) = self.rec_max_path_sum(node.id_right);
+            let (best_l, max_l) = self.rec_max_path_sum_backup(node.id_left);
+            let (best_r, max_r) = self.rec_max_path_sum_backup(node.id_right);
             let path: u32 = node.key + max_l + max_r;
             let best: u32 = max(path, max(best_l, best_r));
             let max: u32 = max(max_l, max_r) + node.key;
@@ -119,7 +117,77 @@ impl Tree {
         }
         (0, 0)
     }
+
+    pub fn max_path_sum(&self) -> Option<u32> {
+        self.rec_max_path_sum(Some(0)).0
+    }
+
+    fn rec_max_path_sum(&self, node_id: Option<usize>) -> (Option<u32>, Option<u32>) {
+        if let Some(id) = node_id {
+            assert!(id < self.nodes.len(), "Node id is out of range");
+            let node: &Node = &self.nodes[id];
+            let (best_l, max_l) = self.rec_max_path_sum(node.id_left);
+            let (best_r, max_r) = self.rec_max_path_sum(node.id_right);
+
+            print!("I'm node: {}, receiving: ", node.key);
+            println!("{}, {}, {}, {}", best_l.unwrap_or(0), max_l.unwrap_or(0),
+                   best_r.unwrap_or(0), max_r.unwrap_or(0));
+
+            match (best_l, max_l, best_r, max_r) {
+                // if everything is defined
+                (Some(bl), Some(ml), Some(br), Some(mr)) => {
+                    let path: u32 = node.key + ml + mr;
+                    let best: u32 = max(path, max(bl, br));
+                    let max: u32 = max(ml, mr) + node.key;
+                    return (Some(best), Some(max));
+                },
+                // if i don't found a best path on left subtree
+                (None, Some(ml), Some(br), Some(mr)) => {
+                    let path: u32 = node.key + ml + mr;
+                    let best: u32 = max(path, br);
+                    let max: u32 = max(ml, mr) + node.key;
+                    return (Some(best), Some(max));
+                },
+                // if i don't found a best path on right subtree
+                (Some(bl), Some(ml), None, Some(mr)) => {
+                    let path: u32 = node.key + ml + mr;
+                    let best: u32 = max(path, bl);
+                    let max: u32 = max(ml, mr) + node.key;
+                    return (Some(best), Some(max));
+                },
+                // if i don't found a best so far
+                (None, Some(ml), None, Some(mr)) => {
+                    let best: u32 = node.key + ml + mr;
+                    let max: u32 = max(ml, mr) + node.key;
+                    return (Some(best), Some(max));
+                },
+                // if i don't have both best so far and max from right child
+                (Some(bl), Some(ml), None, None) => {
+                    return (Some(bl),Some(ml + node.key));
+                }
+                // if i don't have both best so far and max from left child
+                (None, None, Some(br), Some(mr)) => {
+                    return (Some(br),Some(mr + node.key));
+                }
+                // if i only have max from left
+                (None, Some(ml), None, None) => {
+                    return (None,Some(ml + node.key));
+                }
+                // if i only have max from right
+                (None, None, None, Some(mr)) => {
+                    return (None,Some(mr + node.key));
+                }
+                // if i'm a leaf
+                (None, None, None, None) => {
+                    return (None, Some(node.key));
+                }
+                _ => unreachable!("This code should never be reached"),
+            }
+        }
+        (None, None)
+    }
 }
+
 
 /* ---------- Unit Tests ---------- */
 
@@ -140,6 +208,11 @@ mod tests {
 
         bst.add_node(1, 3, true); // id 3
         bst.add_node(1, 9, false); // id 4
+
+        assert!(bst.is_bst(), "Tree is a BST");
+
+        bst.add_node(2, 23, true); // id 5
+        bst.add_node(2, 37, false); // id 6
 
         assert!(bst.is_bst(), "Tree is a BST");
 
@@ -179,43 +252,72 @@ mod tests {
 
     #[test]
     fn test_max_path_sum() {
+
+        // No max path test for the max_path_sum method
         let mut tree = Tree::with_root(20); // id 0
         assert_eq!(
-            tree.max_path_sum(), 20,
-            "Tree with only root must have a max path sum of 20"
+            tree.max_path_sum(),
+            None,
+            "Tree with only root must return None path"
         );
 
         tree.add_node(0, 6, true); // id 1
+        assert_eq!(
+            tree.max_path_sum(),
+            None,
+            "Tree with only root and one node must return None path"
+        );
+
+        tree.add_node(1, 5, true); // id 2
+        assert_eq!(
+            tree.max_path_sum(),
+            None,
+            "Tree with only one leaf must return None path"
+        );
+
+        tree.add_node(2, 3, false); // id 3
+        assert_eq!(
+            tree.max_path_sum(),
+            None,
+            "Tree with only one leaf must return None path"
+        );
+
+        // Standard test for the max_path_sum method
+
+        let mut tree = Tree::with_root(20); // id 0
+        tree.add_node(0, 6, true); // id 1
         tree.add_node(0, 5, false); // id 2
         assert_eq!(
-            tree.max_path_sum(), 31,
-            "Tree with only root and two children must have a max path sum of 31"
+            tree.max_path_sum().unwrap(),
+            31,
+            "This tree has max path sum of 31"
         );
 
-        tree.add_node(1, 0, true); // id 3
-        tree.add_node(1, 0, false); // id 4
+        tree.add_node(2, 9, true); // id 3
+        tree.add_node(2, 8, false); // id 4
         assert_eq!(
-            tree.max_path_sum(), 31,
-            "Tree with only root and two children must have a max path sum of 31"
+            tree.max_path_sum().unwrap(),
+            40,
+            "This tree has max path sum of 40"
         );
 
-        tree.add_node(2, 4, true); // id 5
+        tree.add_node(1, 0, true); // id 5
+        tree.add_node(1, 2, false); // id 6
+
         assert_eq!(
-            tree.max_path_sum(), 35,
-            "Tree with only root and two children must have a max path sum of 35"
+            tree.max_path_sum().unwrap(),
+            42,
+            "This tree has max path sum of 42"
         );
 
-        tree.add_node(2, 30, false); // id 6
+        tree.add_node(3, 55, true); // id 5
+        tree.add_node(3, 150, false); // id 6
+
         assert_eq!(
-            tree.max_path_sum(), 61,
-            "Tree with only root and two children must have a max path sum of 61"
+            tree.max_path_sum().unwrap(),
+            214,
+            "This tree has max path sum of 214"
         );
 
-        tree.add_node(6, 50, true); // id 9
-        tree.add_node(6, 50, false); // id 10
-        assert_eq!(
-            tree.max_path_sum(), 130,
-            "Tree with only root and two children must have a max path sum of 130"
-        );
     }
 }
